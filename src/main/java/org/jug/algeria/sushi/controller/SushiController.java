b567a3f0395855d3ccef85daa3e12623a2033bb4
bouadma.abderrazak@gmail.com <bouadma.abderrazak@gmail.com>
2015-02-06 14:01:00 +0100

    added MvcController for root context. Added Mvc Configuration section into application.properties. Used different context for SushiController as it's now my API resource

--- src/main/java/org/jug/algeria/sushi/controller/SushiController.java
+++ src/main/java/org/jug/algeria/sushi/controller/SushiController.java
@@ -1,13 +1,15 @@
 package org.jug.algeria.sushi.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/api")
 public class SushiController  {
 
-	@RequestMapping(value="/", method=RequestMethod.GET)
+	@RequestMapping(value="/hello", method=RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public String sayHello(){
 		return "Hello There ";
 	}
